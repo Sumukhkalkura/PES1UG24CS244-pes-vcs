b7be8bdb095571e62e198713a1790253836a662c
Sumukhkalkura <sumukhkalkura@gmail.com>
2026-04-21 13:52:49 +0000

    Implemented recursive tree building

--- tree.c
+++ tree.c
@@ -103,7 +103,9 @@ int tree_serialize(const Tree *tree, void **data_out, size_t *len_out) {
     return 0;
 }
 
+
 // ─── TODO: Implement these ──────────────────────────────────────────────────
+
 
 int tree_from_index(ObjectID *id_out) {
     Tree tree;
